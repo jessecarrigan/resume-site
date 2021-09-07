b965bccd493c5eb90aff39d9e8bcfebdc6198ed1
Jesse Carrigan <jesse.carrigan@kasasa.com>
2021-09-06 21:06:17 -0500

    Fixed incorrect variable reference.

--- public/counter.js
+++ public/counter.js
@@ -1,19 +1,19 @@
 window.onload = async function() {
     console.log("Getting views...");
-    const getViews = await fetch("https://2c3xm80q8e.execute-api.us-west-2.amazonaws.com/views")
+    const views = await fetch("https://2c3xm80q8e.execute-api.us-west-2.amazonaws.com/views")
     .then(response => response.json())
     .then(data => {
         let count = data.Item.count.N;
         let views = parseInt(count);
-        return views
+        return views;
     });
 
     const counterElement = document.getElementById("counter");
-    counterElement.textContent = "" + count;
+    counterElement.textContent = "" + views;
 
     body = {
         count: count
-    }
+    };
 
     console.log("Updating views...")
     const update = await fetch("https://2c3xm80q8e.execute-api.us-west-2.amazonaws.com/views", {
